0a4d2b70c098e6fce594a57ea5f39ed0733aacf6
conchlin <connorbeckett@protonmail.com>
2023-07-17 18:52:19 -0400

    port of MapleAESOFB

--- src/main/java/net/mina/MaplePacketEncoder.java
+++ src/main/java/net/mina/MaplePacketEncoder.java
@@ -25,12 +25,12 @@ import constants.ServerConstants;
 import client.MapleClient;
 import constants.OpcodeConstants;
 import net.server.coordinator.MapleSessionCoordinator;
+import network.crypto.ClientEncryption;
 import network.encryption.Shanda;
 import org.apache.mina.core.buffer.IoBuffer;
 import org.apache.mina.core.session.IoSession;
 import org.apache.mina.filter.codec.ProtocolEncoder;
 import org.apache.mina.filter.codec.ProtocolEncoderOutput;
-import tools.MapleAESOFB;
 import tools.HexTool;
 import tools.data.input.ByteArrayByteStream;
 import tools.data.input.GenericLittleEndianAccessor;
@@ -45,7 +45,7 @@ public class MaplePacketEncoder implements ProtocolEncoder {
         try {
             client.lockEncoder();
             try {
-                final MapleAESOFB send_crypto = client.getSendCrypto();
+                final ClientEncryption send_crypto = client.getSendCrypto();
                 final byte[] input = (byte[]) message;
                 if (ServerConstants.USE_DEBUG_SHOW_PACKET) {
                     int packetLen = input.length;
@@ -63,17 +63,16 @@ public class MaplePacketEncoder implements ProtocolEncoder {
                         FilePrinter.print(FilePrinter.PACKET_STREAM + MapleSessionCoordinator.getSessionRemoteAddress(session) + ".txt", HexTool.toString(new byte[]{input[0], input[1]}) + " ...");
                     }
                 }
-                
+
                 final byte[] unencrypted = new byte[input.length];
                 System.arraycopy(input, 0, unencrypted, 0, input.length);
                 final byte[] ret = new byte[unencrypted.length + 4];
                 final byte[] header = send_crypto.getPacketHeader(unencrypted.length);
                 Shanda.Companion.encrypt(unencrypted);
-            
                 send_crypto.crypt(unencrypted);
                 System.arraycopy(header, 0, ret, 0, 4);
                 System.arraycopy(unencrypted, 0, ret, 4, unencrypted.length);
-                
+
                 out.write(IoBuffer.wrap(ret));
             } finally {
                 client.unlockEncoder();
@@ -84,7 +83,7 @@ public class MaplePacketEncoder implements ProtocolEncoder {
             out.write(IoBuffer.wrap(((byte[]) message)));
         }
     }
-    
+
     private String lookupRecv(int val) {
         return OpcodeConstants.sendOpcodeNames.get(val);
     }
@@ -94,5 +93,6 @@ public class MaplePacketEncoder implements ProtocolEncoder {
     }
 
     @Override
-    public void dispose(IoSession session) throws Exception {}
+    public void dispose(IoSession session) throws Exception {
+    }
 }
